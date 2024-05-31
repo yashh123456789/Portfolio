f5bbdd819695638b7a3d65c7530e5afb36336123
Yasiru Ediriweera <143874761+yashh123456789@users.noreply.github.com>
2024-05-31 14:21:18 +0530

    create navigation bar

---
+++ secondone/src/Components/NavBar.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import {BrowserRouter as Router} from "react-router-dom";
+
+
+
+export const NavBar = () => {
+
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+  
+    useEffect(() => {
+      const onScroll = () => {
+        if (window.scrollY > 50) {
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      }
+  
+      window.addEventListener("scroll", onScroll);
+  
+      return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+  
+    const onUpdateActiveLink = (value) => {
+      setActiveLink(value);
+    }
+    return(
+      <Router>
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+          <Container>
+            <Navbar.Brand href="home">
+              <h2 style={{color:"white", fontWeight:"bolder"}}>Portfolio</h2>
+            </Navbar.Brand>
+            
+             
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                <Nav.Link href="#contact" className={activeLink == 'contact' ? 'active navbar-Link' : 'navbar-link' } onClick={() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+       </Navbar>
+     </Router>
+    )
+  }
