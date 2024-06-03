1aa9556bc5c8579b8a28875d9f37a3788f857ee3
Yasiru Ediriweera <143874761+yashh123456789@users.noreply.github.com>
2024-06-04 03:00:01 +0530

    create contact section

--- secondone/src/App.js
+++ secondone/src/App.js
@@ -3,6 +3,7 @@ import  {NavBar} from "../src/Components/NavBar";
 import {Banner} from "../src/Components/Banner";
 import {Projects} from "../src/Components/Projects";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Contact } from "../src/Components/Contact";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavBar/>
       <Banner/>
       <Projects/>
+      <Contact/>
 
 
     </div>
